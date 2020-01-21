eff551c568f6f2c6ba11daf9685d1141289d4ec1
ariyon <ari.h.yonaty@gmail.com>
2020-01-21 12:04:23 -0800

    Latest

---
+++ CSharp/consoleproject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace consoleproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello Everyone!";
+            Console.WriteLine(greeting);
+
+            string name;
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
