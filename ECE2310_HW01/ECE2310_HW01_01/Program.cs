7629d94ef1fc6a162a30fc7382df00477cd425f0
ariyon <ari.h.yonaty@gmail.com>
2020-02-10 21:43:36 -0800

    Reorganizing..

---
+++ ECE2310_HW01/ECE2310_HW01_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ECE2310_HW01_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            double miles, kilometers;
+
+            Console.WriteLine("Enter Miles.");
+            Console.Write("> ");
+            userInput = Console.ReadLine();
+            miles = Convert.ToDouble(userInput);
+            kilometers = miles * 1.60934;
+
+            Console.WriteLine("Miles: " + miles);
+            Console.WriteLine("Kilometers: " + kilometers);
+        }
+    }
+}
