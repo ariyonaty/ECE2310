4d593fbf6c50aeecb507f5cd2e3929c7fd7d530b
ariyonaty <ayonaty@cpp.edu>
2020-02-11 11:06:27 -0800

    Organization

---
+++ Classwork/Week0401/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Week0401
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random n_obj = new Random();
+            int x = n_obj.Next(1,101);
+            Console.WriteLine(x);
+        }
+    }
+}
