680861fbb572074646b29cee4db7ec5098e1bacb
ariyonaty <ayonaty@cpp.edu>
2020-02-18 12:41:23 -0800

    HW 4, Problem 1

---
+++ Classwork/Week0501/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Week0501
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            accumulator(100, '+');
+            accumulator(15, '*');
+            accumulator(15, '^');
+
+            Console.WriteLine();
+
+            double moneyIn = 5000;
+            int years = 10;
+
+            double quarterly = moneyIn;
+            double yearly = moneyIn;
+            for (int i = 1; i <= years * 4; i++)
+            {
+                quarterly += quarterly * 0.005;                    
+            }
+            for (int i = 1; i <= years; i++)
+            {
+                yearly += yearly * 0.02;
+            }
+
+            Console.WriteLine($"Investment balance after {years} year.");
+            Console.WriteLine($"Yearly investment balance: {yearly:C2}");
+            Console.WriteLine($"Quarterly investment balance: {quarterly:C2}");
+
+        }
+
+        private static void accumulator(int n, char operation)
+        {
+            long t = 1;
+            double x = 2;
+
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine("Addition");
+                    for (int i = 1; i <= n; i++)
+                    {
+                        t += i;
+                    }
+                    Console.WriteLine($"T = {t}");
+                    break;
+                case '*':
+                    Console.WriteLine("Factorial");
+                    for (int i = 1; i <= n; i++)
+                    {
+                        t *= i;
+                    }
+                    Console.WriteLine($"{n}! = {t}");
+                    break;
+                case '^':
+                    Console.WriteLine("Power");
+                    for (int i = 1; i < n; i++)
+                    {
+                       x *= 2;
+                    }
+                    Console.WriteLine($"2^{n} = {x}");
+                    break;
+            }
+        }
+
+
+    }
+}
---
+++ HW04/ECE2310_HW04_01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ECE2310_HW04_01
+{
+    class Program
+    {
+        private static int[] fib(int n)
+        {
+            int[] fibonacci = new int[n];
+            fibonacci[0] = 1;
+            fibonacci[1] = 1;
+
+            for (int i = 2; i < n; i++)
+            {
+                fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+            }
+
+            return fibonacci;
+        }
+        static void Main(string[] args)
+        {
+            int[] fibonacci = fib(30);
+
+            int count = 0;
+
+
+            Console.WriteLine("-----------FIBONACCI NUMBERS-----------");
+            for (int i = 1; i <= 6; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write($"{fibonacci[count]}\t ");
+                    count++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
