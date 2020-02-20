4d43187073312dcadce6d284b8fae979e1e7700a
ariyonaty <ayonaty@cpp.edu>
2020-02-20 11:07:27 -0800

    Classwork - Base to Power with listbox.

---
+++ Classwork/Week0502/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Week0502
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.txtPower = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lstPowers = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Base:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Power:";
+            // 
+            // txtBase
+            // 
+            this.txtBase.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBase.Location = new System.Drawing.Point(125, 37);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(99, 20);
+            this.txtBase.TabIndex = 2;
+            // 
+            // txtPower
+            // 
+            this.txtPower.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPower.Location = new System.Drawing.Point(125, 81);
+            this.txtPower.Name = "txtPower";
+            this.txtPower.Size = new System.Drawing.Size(99, 20);
+            this.txtPower.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(46, 152);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(178, 57);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lstPowers
+            // 
+            this.lstPowers.FormattingEnabled = true;
+            this.lstPowers.Location = new System.Drawing.Point(50, 322);
+            this.lstPowers.Name = "lstPowers";
+            this.lstPowers.Size = new System.Drawing.Size(174, 186);
+            this.lstPowers.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(95, 514);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(46, 232);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 57);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Minimal Power for base^power > 10000";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 560);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstPowers);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtPower);
+            this.Controls.Add(this.txtBase);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Exponentiation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.TextBox txtPower;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ListBox lstPowers;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Classwork/Week0502/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week0502
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double inBase;
+            int power;
+
+            if (!double.TryParse(txtBase.Text, out inBase) || inBase == 0)
+            {
+                MessageBox.Show("Error. Base must be a numeric value.", "Invalid Input!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                clearForm();
+                return;
+            }
+
+            if (!Int32.TryParse(txtPower.Text, out power) || power <= 0)
+            {
+                MessageBox.Show("Error. Power must be a number greater than 0.", "Invalid Input!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                clearForm();
+                return;
+            }
+
+            clearForm();
+
+            double origBase = inBase;
+
+            for (int i = 1; i < power; i++)
+            {
+                inBase *= origBase;
+                lstPowers.Items.Add(inBase.ToString());
+                
+            }
+            lstPowers.Items.Add($"{origBase} to the power of {power} is {inBase}");
+        }
+
+        private void clearForm()
+        {
+            txtBase.Clear();
+            txtPower.Clear();
+            lstPowers.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clearForm();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= lstPowers.Items.Count; i++)
+            {
+                if (Convert.ToDouble(lstPowers.Items[i].ToString()) > 10000)
+                {
+                    MessageBox.Show($"First base^power greater than 10000 is {i+1}", "Power Check", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+            }
+        }
+    }
+}
