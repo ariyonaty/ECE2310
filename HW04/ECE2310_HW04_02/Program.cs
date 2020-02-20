2ded39bb23eb6bbc2a54a10b8ac58023b13e9bc1
ariyonaty <ayonaty@cpp.edu>
2020-02-20 11:59:15 -0800

    HW4 In progress.

---
+++ HW04/ECE2310_HW04_02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ECE2310_HW04_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            menu();
+
+            double startMoney = 1000;
+            int startAge = 20;
+
+            double john = startMoney;
+            double linda = startMoney;
+        }
+        private static void menu()
+        {
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("                 Investing vs Saving                 ");
+            Console.WriteLine("-----------------------------------------------------");
+
+            Console.WriteLine();
+            Console.WriteLine("Age      Linda's Account         John's Account      ");
+            Console.WriteLine("-----------------------------------------------------");
+        }
+    }
+}
