ff96b064ad700219179d1c9bf59dac2586ed36ab
ariyon <ari.h.yonaty@gmail.com>
2020-02-21 00:57:45 -0800

    HW04 Complete

---
+++ HW04/ECE2310_HW04_02/ECE2310_HW04_02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ECE2310_HW04_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            menu();
+
+            double startMoney = 1000;
+            int startAge = 20;
+            int age = startAge;
+
+            double linda = startMoney;
+            double john = startMoney;
+
+            double lindaRate = 0.06;
+            double johnRate = 0.015;
+
+            for (int i = 20; i <= 60; i++)
+            {
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine($"{age,-13}{linda,-24:C2}{john,-10:C2}");
+                    age += 10;
+                }
+
+                linda += linda * lindaRate;
+                john += john * johnRate;
+            }
+        }
+
+        private static void menu()
+        {
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("                       Investing vs Saving                       ");
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Age          Linda's Account         John's Account              ");
+            Console.WriteLine("-----------------------------------------------------------------");
+        }
+    }
+}
---
+++ HW04/ECE2310_HW04_03/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ECE2310_HW04_03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            int n;
+            int i = 1, factorial = 1;
+            double eVal = 1.0;
+
+            if (!Int32.TryParse(txtN.Text, out n) || n < 0)
+            {
+                MessageBox.Show("Error, input must be positive number!", "Input Invalid!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                clearForm();
+                return;
+            }
+
+            lstFactorial.Items.Clear();
+
+            if (n == 0 || n == 1)
+            {
+                lstFactorial.Items.Add($"{n}! = {factorial:n0}");
+                return;
+            }
+
+            while (i <= n)
+            {
+                factorial *= i;
+                lstFactorial.Items.Add($"{i}! = {factorial:n0}");
+                eVal += (double)(1.0 / factorial);
+                i++;
+            }
+            lstFactorial.Items.Add($"e approximation: {Math.Round(eVal, 6)}");
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            clearForm();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void clearForm()
+        {
+            txtN.Clear();
+            lstFactorial.Items.Clear();
+        }
+    }
+}
