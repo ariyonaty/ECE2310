42fe534dea1c943a7d75150daf46d4c4a6095a6d
ariyonaty <ayonaty@cpp.edu>
2020-02-25 12:53:18 -0800

    TODO: Fix HW 5, problem 2

---
+++ Classwork/Week0601/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Week0601
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[6];
+
+            int sum = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    a[i] = (i + 1) * 10;
+                }
+                else
+                {
+                    a[i] = (i + 1) * 10 * 2;
+                }
+                sum += a[i];
+                
+            }
+            printArray(a);
+            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Average = {sum / a.Length}");
+        }
+
+        private static void printArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Index {i} ; Value = {array[i]}");
+            }
+        }
+    }
+}
---
+++ HW05/ECE2310_HW05_02/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ECE2310_HW05_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[5];
+            int[] grades = new int[5];
+
+            int sum = 0;
+            int max = 0;
+            int avgCount = 0;
+            int maxIndex = 0;
+
+            Console.WriteLine("*****************************");
+            Console.WriteLine("     Class Grade System");
+            Console.WriteLine("*****************************");
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.Write("Enter name > ");
+                names[i] = Console.ReadLine();
+
+                Console.Write("Enter grade > ");
+                while(!Int32.TryParse(Console.ReadLine(), out grades[i]) || grades[i] > 100 || grades[i] < 0)
+                {
+                    Console.Write("Invalid input. Try again > ");
+                }
+                sum += grades[i];
+                if(grades[i] > max)
+                {
+                    max = grades[i];
+                }
+            }
+
+            Console.WriteLine("*****************************");
+            Console.WriteLine("Grade            Score");
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"{names[i],-16} {grades[i],-13}");
+            }
+
+            maxIndex = Array.IndexOf(names, max);
+            int avg = sum / grades.Length;
+
+            Console.WriteLine("*****************************");
+            Console.WriteLine($"{names[maxIndex]} has the highest grade, {max}");
+            Console.WriteLine($"Average class grade > {avg}");
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                if (grades[i] > avg)
+                {
+                    avgCount++;
+                }
+            }
+
+            Console.WriteLine($"{avgCount} students are above average score.");
+        }
+    }
+}
