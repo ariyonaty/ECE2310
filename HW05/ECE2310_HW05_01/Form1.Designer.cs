2da78e664a29c54189e16b745443ef97e1ba5e1f
ariyonaty <ayonaty@cpp.edu>
2020-02-25 12:30:36 -0800

    HW 5, prob 1, done.

---
+++ HW05/ECE2310_HW05_01/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ECE2310_HW05_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRand = new System.Windows.Forms.Button();
+            this.lstRand = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRand
+            // 
+            this.btnRand.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRand.Location = new System.Drawing.Point(21, 24);
+            this.btnRand.Name = "btnRand";
+            this.btnRand.Size = new System.Drawing.Size(275, 51);
+            this.btnRand.TabIndex = 0;
+            this.btnRand.Text = "Generate Random Numbers";
+            this.btnRand.UseVisualStyleBackColor = true;
+            this.btnRand.Click += new System.EventHandler(this.btnRand_Click);
+            // 
+            // lstRand
+            // 
+            this.lstRand.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lstRand.FormattingEnabled = true;
+            this.lstRand.Location = new System.Drawing.Point(26, 98);
+            this.lstRand.Name = "lstRand";
+            this.lstRand.Size = new System.Drawing.Size(270, 238);
+            this.lstRand.TabIndex = 1;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.Control;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(26, 363);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(119, 51);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.Control;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(177, 363);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(119, 51);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 426);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lstRand);
+            this.Controls.Add(this.btnRand);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Random Number Generator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRand;
+        private System.Windows.Forms.ListBox lstRand;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ HW05/ECE2310_HW05_01/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ECE2310_HW05_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRand_Click(object sender, EventArgs e)
+        {
+            lstRand.Items.Clear();
+            Random rand = new Random();
+            int r60 = 0, r60to70 = 0, r70to80 = 0, r80to90 = 0, r90to100 = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                double r = rand.NextDouble() * (100 - 40) + 40;
+                lstRand.Items.Add(Math.Round(r, 1));
+                checkRange(ref r60, ref r60to70, ref r70to80, ref r80to90, ref r90to100, r);
+            }
+            lstRand.Items.Add("");
+            lstRand.Items.Add("Range                Count");
+            lstRand.Items.Add("--------------------------------------");
+            lstRand.Items.Add($"Below 60            {r60}");
+            lstRand.Items.Add($"[60, 70)              {r60to70}");
+            lstRand.Items.Add($"[70, 80)              {r70to80}");
+            lstRand.Items.Add($"[80, 90)              {r80to90}");
+            lstRand.Items.Add($"[90, 100)]           {r90to100}");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstRand.Items.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void checkRange(ref int r60, ref int r60to70, ref int r70to80, ref int r80to90, ref int r90to100, double r)
+        {
+            if (r >= 90)
+            {
+                r90to100++;
+            }
+            else if (r >= 80)
+            {
+                r80to90++;
+            }
+            else if (r >= 70)
+            {
+                r70to80++;
+            }
+            else if (r >= 60)
+            {
+                r60to70++;
+            }
+            else
+            {
+                r60++;
+            }
+        }
+    }
+}
