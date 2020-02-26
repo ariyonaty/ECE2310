721bd1e4d0f91af6c73140dd00cb9e88d3b4e8a4
ariyon <ari.h.yonaty@gmail.com>
2020-02-25 20:54:49 -0800

    Completed HW 5.

--- HW05/ECE2310_HW05_02/Program.cs
+++ HW05/ECE2310_HW05_02/Program.cs
@@ -6,8 +6,8 @@ namespace ECE2310_HW05_02
     {
         static void Main(string[] args)
         {
-            string[] names = new string[5];
-            int[] grades = new int[5];
+            string[] names = new string[26];
+            int[] grades = new int[26];
 
             int sum = 0;
             int max = 0;
@@ -24,12 +24,12 @@ namespace ECE2310_HW05_02
                 names[i] = Console.ReadLine();
 
                 Console.Write("Enter grade > ");
-                while(!Int32.TryParse(Console.ReadLine(), out grades[i]) || grades[i] > 100 || grades[i] < 0)
+                while (!Int32.TryParse(Console.ReadLine(), out grades[i]) || grades[i] > 100 || grades[i] < 0)
                 {
                     Console.Write("Invalid input. Try again > ");
                 }
                 sum += grades[i];
-                if(grades[i] > max)
+                if (grades[i] > max)
                 {
                     max = grades[i];
                 }
@@ -42,12 +42,12 @@ namespace ECE2310_HW05_02
                 Console.WriteLine($"{names[i],-16} {grades[i],-13}");
             }
 
-            maxIndex = Array.IndexOf(names, max);
+            maxIndex = Array.IndexOf(grades, max);
             int avg = sum / grades.Length;
 
             Console.WriteLine("*****************************");
-            Console.WriteLine($"{names[maxIndex]} has the highest grade, {max}");
-            Console.WriteLine($"Average class grade > {avg}");
+            Console.WriteLine($"{names[maxIndex]} has the highest grade, {max}, in class.");
+            Console.WriteLine($"Average class grade is {avg}");
 
             for (int i = 0; i < grades.Length; i++)
             {
