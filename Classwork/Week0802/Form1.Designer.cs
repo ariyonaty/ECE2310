2a4ba7f9b26799abd440879b5ff6e6b0c36615ff
ariyonaty <ayonaty@cpp.edu>
2020-03-12 11:20:35 -0700

    Randomize with methods.

---
+++ Classwork/Week0802/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Week0802
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.btnRand = new System.Windows.Forms.Button();
+            this.btnAvg = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(142, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(340, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "N:";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(57, 23);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(77, 20);
+            this.txtA.TabIndex = 3;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(175, 23);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(77, 20);
+            this.txtB.TabIndex = 4;
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(375, 23);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(77, 20);
+            this.txtN.TabIndex = 5;
+            // 
+            // btnRand
+            // 
+            this.btnRand.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRand.Location = new System.Drawing.Point(278, 97);
+            this.btnRand.Name = "btnRand";
+            this.btnRand.Size = new System.Drawing.Size(109, 51);
+            this.btnRand.TabIndex = 6;
+            this.btnRand.Text = "Randomize";
+            this.btnRand.UseVisualStyleBackColor = true;
+            this.btnRand.Click += new System.EventHandler(this.btnRand_Click);
+            // 
+            // btnAvg
+            // 
+            this.btnAvg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAvg.Location = new System.Drawing.Point(407, 97);
+            this.btnAvg.Name = "btnAvg";
+            this.btnAvg.Size = new System.Drawing.Size(109, 51);
+            this.btnAvg.TabIndex = 7;
+            this.btnAvg.Text = "Average";
+            this.btnAvg.UseVisualStyleBackColor = true;
+            this.btnAvg.Click += new System.EventHandler(this.btnAvg_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(278, 154);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(109, 51);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(407, 154);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(109, 51);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(44, 78);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(207, 160);
+            this.listBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 277);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAvg);
+            this.Controls.Add(this.btnRand);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Button btnRand;
+        private System.Windows.Forms.Button btnAvg;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Classwork/Week0802/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week0802
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRand_Click(object sender, EventArgs e)
+        {
+            int size = 1;
+            double startRange = 0, endRange = 0;
+            getInput(ref startRange, txtA.Text);
+            getInput(ref endRange, txtB.Text);
+            getInput(ref size, txtN.Text);
+            
+            double[] randArray = new double[size];
+
+            Random rand = new Random();
+            for (int i = 0; i < randArray.Length; i++)
+            {
+                randArray[i] = Math.Round((rand.NextDouble() * (endRange - startRange)) + startRange, 4);
+            }
+
+            displayArray(randArray);
+        }
+
+        private void btnAvg_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clearForm();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void clearForm()
+        {
+            txtA.Clear();
+            txtB.Clear();
+            txtN.Clear();
+            listBox1.Items.Clear();
+        }
+
+        private void displayArray(double[] array)
+        {
+            foreach (double i in array)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void getInput(ref double x, string text)
+        {
+            if (!Double.TryParse(text, out x))
+            {
+                MessageBox.Show("Error, input must be a number.", "Input Validation!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                clearForm();
+            }
+        }
+
+        private void getInput(ref int x, string text)
+        {
+            if (!Int32.TryParse(text, out x) && x < 0)
+            {
+                MessageBox.Show("Error, size must be positive number.", "Input Validation!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                clearForm();
+                return;
+            }
+            return;
+        }
+    }
+}
