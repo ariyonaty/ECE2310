9821adcc18c47d536a79b73ba127b12ee63d0d10
ariyon <ari.h.yonaty@gmail.com>
2020-03-23 21:55:49 -0700

    Completed HW 7

---
+++ HW07/ECE2310_HW07_02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ECE2310_HW07_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseN = 2;
+            int power = 10;
+            int sum = 1;
+
+            Console.WriteLine(" --------------------------");
+            Console.WriteLine("|      Sigma & Power       |");
+            Console.WriteLine(" --------------------------");
+
+            Console.WriteLine("[+] Calculating :");
+            Console.Write($"> {sum}");
+            for (int i = 1; i <= power; i++)
+            {
+                sum += integerPower(baseN, i);
+                Console.Write($" + {integerPower(baseN, i)}");
+            }
+            Console.WriteLine($" = {sum}");
+            Console.WriteLine(" --------------------------");
+            Console.WriteLine("Goodbye.");
+        }
+
+        static int integerPower(int baseN, int exponent)
+        {
+            // baseN ^ exponent
+            int res = 1;
+
+            for (int i = 0; i < exponent; i++)
+            {
+                res = res * baseN;
+
+            }
+
+            return res;
+        }
+    }
+}
---
+++ HW07/ECE2310_HW07_03/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace ECE2310_HW07_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ------------------------------------------");
+            Console.WriteLine("|            Net Pay Calculator            |");
+            Console.WriteLine(" ------------------------------------------");
+
+            bool again = false;
+
+            do
+            {
+                Console.Write("Employee Name? ");
+                string name = Console.ReadLine();
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[+] Employee Name: {name}");
+                Console.ResetColor();
+                Console.Write("Employee Weekly Sales? $");
+                double sales = input();
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[+] Weekly Salary: {sales:C2}");
+                Console.ResetColor();
+                Console.WriteLine(" ------------------------------------------");
+                Console.Write("Determine net? (y/n) ");
+                if (Console.ReadLine() == "y")
+                {
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    Console.WriteLine(" ------------------------------------------");
+                    calculate(sales);
+                }
+                else
+                {
+                    Console.WriteLine(" ------------------------------------------");
+                    Console.WriteLine("Goodbye.");
+                    Environment.Exit(0);
+                }
+                Console.WriteLine(" ------------------------------------------");
+                Console.Write("Again? (y/n) ");
+                char againInput = Convert.ToChar(Console.ReadLine());
+                if (againInput == 'y')
+                {
+                    again = true;
+                }
+                else
+                {
+                    Console.WriteLine(" ------------------------------------------");
+                    Console.WriteLine("Goodbye.");
+                    Environment.Exit(0);
+                }
+            } while (again);
+
+        }
+
+        static double input()
+        {
+            double input = 0;
+            while (!Double.TryParse(Console.ReadLine(), out input) || input < 0)
+            {
+                Console.Write("[-] Invalid Input. Try again > ");
+            }
+            return input;
+        }
+
+        static void calculate(double sales)
+        {
+            double net;
+            double deducFed, deducRet, deducSoc;
+
+            double federalTax = 0.18;
+            double retirementCont = 0.15;
+            double socialSec = 0.09;
+
+            deducFed = Math.Round(sales * federalTax, 2);
+            deducRet = Math.Round(sales * retirementCont, 2);
+            deducSoc = Math.Round(sales * socialSec, 2);
+
+            net = sales - deducFed - deducRet - deducSoc;
+            display(deducFed, deducRet, deducSoc, net);
+        }
+        static void display(double deducFed, double deducRet, double deducSoc, double net)
+        {
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.WriteLine("[+] Deductions: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"- Federal Tax (18%)             >  {deducFed:C2}");
+            Console.WriteLine($"- Retirement Contribution (15%) >  {deducRet:C2}");
+            Console.WriteLine($"- Social Security (9%)          >  {deducSoc:C2}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"---> Net Pay                    >  {net:C2}");
+            Console.ResetColor();
+        }
+    }
+}
