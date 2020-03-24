427a0e709bdf7dbe2e24bfc7c43024694717e667
ariyon <ari.h.yonaty@gmail.com>
2020-03-24 10:44:45 -0700

    Complete Midterm 2

---
+++ Midterm02/ECE2310_Midterm2/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ECE2310_Midterm2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSep = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSep
+            // 
+            this.btnSep.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSep.Location = new System.Drawing.Point(12, 27);
+            this.btnSep.Name = "btnSep";
+            this.btnSep.Size = new System.Drawing.Size(193, 37);
+            this.btnSep.TabIndex = 0;
+            this.btnSep.Text = "Separate Numbers";
+            this.btnSep.UseVisualStyleBackColor = true;
+            this.btnSep.Click += new System.EventHandler(this.btnSep_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(12, 270);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(91, 33);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(114, 270);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(91, 33);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.IpAddress;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "How Many Numbers?";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(18, 136);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(186, 108);
+            this.listBox1.TabIndex = 4;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(130, 93);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(74, 20);
+            this.txtInput.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(217, 315);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSep);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "ECE2310 Midterm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSep;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ Midterm02/ECE2310_Midterm2/Form1.cs
@@ -0,0 +1,81 @@
+﻿/*
+ Ari Yonaty
+ ECE 2310
+ 3.24.2020
+ Midterm 2
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ECE2310_Midterm2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSep_Click(object sender, EventArgs e)
+        {
+            int n;
+
+            if (!Int32.TryParse(txtInput.Text, out n) || n < 0)
+            {
+                MessageBox.Show("Error, input must be positive number.", "Input Invalid!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                clearForm();
+                return;
+            }
+
+            Random rand = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                seperate(rand.Next(0, 100000));
+            }
+
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clearForm();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void seperate(int num)
+        {
+            int n1 = (num % 100000) / 10000;
+            int n2 = (num % 10000) / 1000;
+            int n3 = (num % 1000) / 100;
+            int n4 = (num % 100) / 10;
+            int n5 = (num % 10);
+
+            int sum = n1 + n2 + n3 + n4 + n5;
+            listBox1.Items.Add($"{num} --> {n1} + {n2} + {n3} + {n4} + {n5} = {sum}");
+        }
+
+        private void clearForm()
+        {
+            listBox1.Items.Clear();
+            txtInput.Clear();
+        }
+    }
+}
