e1f34037ed9b012ed4f76819d6aa439831420114
ariyon <ari.h.yonaty@gmail.com>
2020-05-05 23:06:34 -0700

    A working mess.

--- FinalProject/FinalProject/Driver.cs
+++ FinalProject/FinalProject/Driver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 
 namespace FinalProject
@@ -9,26 +10,6 @@ namespace FinalProject
         {
             Random rand = new Random();
 
-            Location startLocation = new Location(0, 0);
-            /* 
-                        Pool[] pools = new Pool[7];
-                        pools[0] = new Pool(new Location(4, 8), 'A');
-                        pools[1] = new Pool(new Location(1, 3), 'B');
-                        pools[2] = new Pool(new Location(4, 2), 'C');
-                        pools[3] = new Pool(new Location(13, 1), 'D');
-                        pools[4] = new Pool(new Location(12, 9), 'E');
-                        pools[5] = new Pool(new Location(10, 5), 'F');
-                        pools[6] = new Pool(new Location(6, 6), 'G');
-
-                        Pool[] unvisitied = new Pool[7];
-                        Pool[] visitied = new Pool[7];
-
-                        for (int i = 0; i < unvisitied.Length; i++)
-                        {
-                            unvisitied[i] = pools[i];
-                        }
-             */
-
             List<Pool> myPools = new List<Pool>();
             myPools.Add(new Pool(new Location(4, 8), 'A'));
             myPools.Add(new Pool(new Location(1, 3), 'B'));
@@ -37,47 +18,45 @@ namespace FinalProject
             myPools.Add(new Pool(new Location(12, 9), 'E'));
             myPools.Add(new Pool(new Location(10, 5), 'F'));
             myPools.Add(new Pool(new Location(6, 6), 'G'));
+            Console.WriteLine("List of all Pools.");
             foreach (Pool pool in myPools)
             {
                 Console.WriteLine(pool);
             }
+            Console.WriteLine("********************");
+
+            Location startLocation = new Location(0, 0);
+            Location currentLocation = startLocation;
 
             List<Pool> unvisitedPools = new List<Pool>(myPools);
             List<Pool> visitedPools = new List<Pool>();
 
-            Console.WriteLine("********************");
-            double min = Pool.GetDistance(startLocation, unvisitedPools[0].Location);
+            double min; //= Pool.GetDistance(currentLocation, unvisitedPools[0].Location);
             Pool minPool = new Pool();
-            Console.WriteLine(min);
-            Console.WriteLine(unvisitedPools.Count);
-            Console.WriteLine(visitedPools.Count);
 
-            foreach (Pool pool in unvisitedPools)
+            while (unvisitedPools.Count > 0)
             {
-                double dist = Pool.GetDistance(startLocation, pool.Location);
-                Console.WriteLine($"Distance from {startLocation} to {pool.Location} is {dist}");
-                if (dist < min)
+                min = 100;
+                foreach (Pool pool in unvisitedPools)
                 {
-                    min = dist;
-                    minPool = pool;
+                    double dist = Pool.GetDistance(currentLocation, pool.Location);
+                    Console.WriteLine($"Distance from {currentLocation} to {pool.Location} is {dist}");
+                    if (dist <= min)
+                    {
+                        min = dist;
+                        minPool = pool;
+                    }
                 }
+                Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^");
+                currentLocation = minPool.Location;
+                // min = Pool.GetDistance(currentLocation, unvisitedPools[0].Location);
+                unvisitedPools.Remove(minPool);
+                visitedPools.Add(minPool);
+                Console.WriteLine("Counter unvisitied: " + unvisitedPools.Count);
+                Thread.Sleep(1000);
             }
 
-            Console.WriteLine("----------------------");
-            foreach (Pool pool in unvisitedPools)
-            {
-                Console.WriteLine(pool);
-            }
-            unvisitedPools.Remove(minPool);
-            visitedPools.Add(minPool);
-            Console.WriteLine("----------------------");
-            foreach (Pool pool in unvisitedPools)
-            {
-                Console.WriteLine(pool);
-            }
-            Console.WriteLine("----------------------");
 
-            Console.WriteLine(min);
             Console.WriteLine("********************");
             Console.WriteLine("Pools to visit.");
             Console.Write(startLocation);
@@ -85,6 +64,7 @@ namespace FinalProject
             {
                 Console.Write(" --> ");
                 Console.Write(pool.Identifier);
+                Console.Write(pool.Location.Position());
             }
             Console.WriteLine();
 
@@ -92,3 +72,4 @@ namespace FinalProject
         }
     }
 }
+
